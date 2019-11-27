e4be9e65a105fd6ee222a897ba4647b6bdfd6ba1
AndersonHTN <56129719+AndersonHTN@users.noreply.github.com>
2019-11-27 19:49:19 -0300

    Add files via upload

---
+++ SmartEscritura/javascript/blockchain.js
@@ -0,0 +1,51 @@
+var enderecoContrato = "0xB9ab4749E6be481CF8A56B9Ca35F924Cf376ded6";
+var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
+ethereum.enable();
+var signatario = provedor.getSigner();
+var contrato = new ethers.Contract(enderecoContrato, abiContrato, signatario);
+
+var CpfComprador = _CpfComprador
+
+
+function registrarMudancaStatus() {
+    var textoCampo = document.frmStatus.txtStatusPagamentoAluguel.value;
+    var caixaStatusTx = document.getElementById("caixaStatusTx");
+    if (textoCampo.length === 8) {
+        caixaStatusTx.innerHTML = "Enviando transação...";
+        contrato.mudaStatusPagamento(textoCampo)
+        .then( (transacao) => {
+            console.log("registrarMudancaStatus - Transacao ", transacao);   
+            caixaStatusTx.innerHTML = "Transação enviada. Aguardando processamento...";
+            transacao.wait()
+            .then( (resultado) => {
+                buscaStatusContrato();
+                caixaStatusTx.innerHTML = "Transação realizada.";
+            })        
+            .catch( (err) => {
+                console.error("registrarMudancaStatus - Aguardando tx ser minerada");
+                console.error(err);
+                caixaStatusTx.innerHTML = "Algo saiu errado: " + err.message;
+            })
+        })
+        .catch( (err) => {
+            console.error("registrarMudancaStatus");
+            console.error(err);
+            caixaStatusTx.innerHTML = "Algo saiu errado: " + err.message;
+        })
+    }
+}
+
+function buscaStatusContrato() {
+    var status;
+    var campoStatus = document.getElementById("campoStatus");     
+    contrato.statusPagamentoAluguel()
+    .then( (resultado) => {
+        campoStatus.innerHTML = resultado;
+    })
+    .catch( (err) => {
+        console.error(err);
+        campoStatus.innerHTML = err;
+    });
+}
+
+
